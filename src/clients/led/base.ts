e7214b68bdadf83169a634e764c8d51e51341560
Andy Dam <andy@andydam.com>
2020-10-31 16:29:57 -0700

    feat: add base client for led

---
+++ src/clients/led/base.ts
@@ -0,0 +1,90 @@
+import * as d3Interpolate from 'd3-interpolate';
+import { TypedEmitter } from 'tiny-typed-emitter';
+
+////////////////////////////////////////////////////////////
+/// INTERFACES
+
+interface BaseEvents {
+  connected: () => void;
+  deviceDisconnected: () => void;
+  disconnected: () => void;
+  reconnected: () => void;
+}
+
+////////////////////////////////////////////////////////////
+/// PRIVATE
+
+const roundUpToNearest5 = (x: number): number => Math.ceil(x / 5) * 5;
+
+const sleep = (delay: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, delay));
+
+////////////////////////////////////////////////////////////
+/// PUBLIC
+
+export abstract class Base extends TypedEmitter<BaseEvents> {
+  address: string;
+
+  brightnessInterval = 5;
+  colorSteps = 20;
+  currentBrightness: number | null = null;
+  currentColor: string | null = null;
+
+  constructor(address: string) {
+    super();
+
+    this.address = address;
+  }
+
+  abstract async start(): Promise<void>;
+
+  abstract async disconnect(): Promise<void>;
+
+  abstract async setPower(power: boolean): Promise<void>;
+
+  abstract async setBrightness(brightness: number): Promise<void>;
+
+  abstract async setColor(color: string): Promise<void>;
+
+  async setBrightnessTransition(brightness: number): Promise<void> {
+    if (!this.currentBrightness) {
+      this.currentBrightness = brightness;
+      return this.setBrightness(brightness);
+    }
+
+    const currentBrightness = roundUpToNearest5(this.currentBrightness);
+    const desiredBrightness = roundUpToNearest5(brightness);
+
+    const interval =
+      brightness > this.currentBrightness
+        ? this.brightnessInterval
+        : -this.brightnessInterval;
+    const numIntervals =
+      (currentBrightness - desiredBrightness) / this.brightnessInterval;
+
+    for (let i = 0; i < numIntervals; i += 1) {
+      await this.setBrightness(desiredBrightness + interval * i);
+    }
+    this.currentBrightness = brightness;
+  }
+
+  async setColorTransition(color: string): Promise<void> {
+    if (!this.currentColor) {
+      this.currentColor = color;
+      return this.setColor(color);
+    }
+
+    const interpolatedColors = d3Interpolate.interpolateHclLong(
+      this.currentColor,
+      color,
+    );
+
+    for (let i = 0; i <= 1; i += 1 / this.colorSteps) {
+      const desiredColor = interpolatedColors(i);
+      await this.setColor(desiredColor);
+      await sleep(50);
+    }
+
+    this.currentColor = color;
+  }
+}
