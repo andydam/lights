5bb518fc192201d38e0776bb67542e21c9ebedff
Andy Dam <andy@andydam.com>
2020-10-31 17:01:16 -0700

    feat: remove sleep from base

--- src/clients/led/base.ts
+++ src/clients/led/base.ts
@@ -1,8 +1,6 @@
 import * as d3Interpolate from 'd3-interpolate';
 import { TypedEmitter } from 'tiny-typed-emitter';
 
-import { sleep } from '../../utils';
-
 ////////////////////////////////////////////////////////////
 /// INTERFACES
 
@@ -66,7 +64,6 @@ export abstract class Base extends TypedEmitter<BaseEvents> {
 
     for (let i = 0; i < numIntervals; i += 1) {
       await this.setBrightness(currentBrightness + interval * i);
-      await sleep(50);
     }
     this.currentBrightness = brightness;
   }
@@ -85,7 +82,6 @@ export abstract class Base extends TypedEmitter<BaseEvents> {
     for (let i = 0; i <= 1; i += 1 / this.colorSteps) {
       const desiredColor = interpolatedColors(i);
       await this.setColor(desiredColor);
-      await sleep(50);
     }
 
     this.currentColor = color;
