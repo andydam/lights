a0f2f4f4fe0df475bff20f8f9bac76c4377eb508
Andy Dam <andy@andydam.com>
2020-11-04 21:39:21 -0800

    feat: add logger

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,25 @@
+import * as winston from 'winston';
+
 ////////////////////////////////////////////////////////////
 /// PUBLIC
 
 export const sleep = (delay: number): Promise<void> =>
   new Promise((resolve) => setTimeout(resolve, delay));
+
+export const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message, ...args }) => {
+      const ts = timestamp.slice(0, 19).replace('T', ' ');
+      return `${ts} ${level}: ${message} ${
+        Object.keys(args).length ? JSON.stringify(args, null, 2) : ''
+      }`;
+    }),
+  ),
+  transports: [
+    new winston.transports.Console({
+      level: 'debug',
+    }),
+  ],
+});
